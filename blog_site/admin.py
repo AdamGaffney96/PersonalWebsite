553c1076ddff857082ac68f797cf2fffe8b160be
Adam Gaffney <adamgaffney96@gmail.com>
2021-07-22 16:46:28 +0100

    MM401 Essay Added

--- blog_site/admin.py
+++ blog_site/admin.py
@@ -3,10 +3,10 @@ from .models import *
 from django_summernote.admin import SummernoteModelAdmin
 
 class GamingAdmin(SummernoteModelAdmin):
-    summernote_fields = ('content', 'thumb', )
+    summernote_fields = ('content', )
 
 class EssayAdmin(SummernoteModelAdmin):
-    summernote_fields = ('content', 'thumb', )
+    summernote_fields = ('content', )
 
 # Register your models here.
 admin.site.register(Gaming, GamingAdmin)
