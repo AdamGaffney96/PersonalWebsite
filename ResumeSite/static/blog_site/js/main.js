382f22a114c4a4b17c09643f85f33d83bae6d3c9
Adam Gaffney <adamgaffney96@gmail.com>
2021-07-22 15:59:17 +0100

    final fix for input boxes

--- ResumeSite/static/blog_site/js/main.js
+++ ResumeSite/static/blog_site/js/main.js
@@ -3,33 +3,19 @@ let ham_nav = document.getElementById('hamburger-nav-list')
 if (screen.width <= 1024) {
     hamburger.addEventListener('touchend', (e) => {
         let content = document.getElementById('hamburger-nav-list');
-        // content.style.width = document.getElementsByTagName('main')[0].offsetWidth + "px"
-        // console.log(content.style.width)
         if (content.style.maxHeight) {
             content.style.maxHeight = null;
-            // setTimeout(function() { document.getElementById("overlay").className = "fadeOutOverlay" }, 1)
-            // setTimeout(function() { document.getElementById("overlay").style.display = "none" }, 301)
         } else {
-            // document.getElementById("overlay").style.display = null
             content.style.maxHeight = content.scrollHeight + "px";
-            // document.getElementById("overlay").classList.remove("fadeOutOverlay")
-            // setTimeout(function() { document.getElementById("overlay").className = "fadeInOverlay" }, 1)
         }
     })
 } else {
     hamburger.addEventListener('click', (e) => {
         let content = document.getElementById('hamburger-nav-list');
-        // content.style.width = document.getElementsByTagName('main')[0].offsetWidth + "px"
-        // console.log(content.style.width)
         if (content.style.maxHeight) {
             content.style.maxHeight = null;
-            // setTimeout(function() { document.getElementById("overlay").className = "fadeOutOverlay" }, 1)
-            // setTimeout(function() { document.getElementById("overlay").style.display = "none" }, 301)
         } else {
-            // document.getElementById("overlay").style.display = null
             content.style.maxHeight = content.scrollHeight + "px";
-            // document.getElementById("overlay").classList.remove("fadeOutOverlay")
-            // setTimeout(function() { document.getElementById("overlay").className = "fadeInOverlay" }, 1)
         }
     })
 }
@@ -40,9 +26,6 @@ function getWidth() {
 
 window.addEventListener('resize', (e) => {
     ham_nav.style.maxHeight = null;
-    // document.getElementById("overlay").style.display = null
-    // document.getElementById("overlay").classList.remove("fadeInOverlay")
-    // document.getElementById("overlay").classList.add("fadeOutOverlay")
 })
 
 // max-device-width for mobile: 1024
