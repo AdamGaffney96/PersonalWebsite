a239a19c697a20d72f5d194e4b6fc023b643e327
Adam Gaffney <adamgaffney96@gmail.com>
2021-07-23 16:10:38 +0100

    force SSL

--- ResumeSite/settings.py
+++ ResumeSite/settings.py
@@ -32,6 +32,9 @@ if os.path.isfile(dotenv_file):
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', '7%%t6c(7kq*m%g#mnv(3e_n0&3ud@2j@p8&shb!-w0#f9u!1c7')
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
