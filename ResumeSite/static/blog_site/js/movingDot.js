90b99e256af1ea80f92a1954d371b7964fd243f0
Adam Gaffney <adamgaffney96@gmail.com>
2021-08-08 13:40:39 +0100

    nav fixes

--- ResumeSite/static/blog_site/js/movingDot.js
+++ ResumeSite/static/blog_site/js/movingDot.js
@@ -1,6 +1,7 @@
 const dotCont = document.getElementById('moving-dot-container')
     // const dotBody = document.getElementById('moving-dot-surround')
 const dotBody = document.getElementById('moving-dot-body')
+
 const dotMove = e => {
     x = dotCont.offsetWidth - dotBody.offsetWidth
     y = dotCont.offsetHeight - dotBody.offsetHeight - 80
@@ -11,7 +12,32 @@ const dotMove = e => {
 }
 
 dotBody.addEventListener('click', (e) => {
-    console.log('pog')
+    x = dotCont.offsetWidth - dotBody.offsetWidth
+    y = dotCont.offsetHeight - dotBody.offsetHeight - 80
+    newTop = Math.floor(Math.random() * y)
+    newLeft = Math.floor(Math.random() * x)
+    if (!dotBody.classList.length) {
+        dotBody.classList.add('clicked1')
+        dotBody.style.backgroundColor = 'yellow'
+        newTop < 80 ? '80px' : dotBody.style.top = newTop + 'px'
+        dotBody.style.left = newLeft + 'px'
+    } else if (dotBody.classList[0] == 'clicked1') {
+        dotBody.classList.remove('clicked1')
+        dotBody.classList.add('clicked2')
+        dotBody.style.backgroundColor = 'orange'
+        newTop < 80 ? '80px' : dotBody.style.top = newTop + 'px'
+        dotBody.style.left = newLeft + 'px'
+    } else if (dotBody.classList[0] == 'clicked2') {
+        dotBody.classList.remove('clicked2')
+        dotBody.classList.add('clicked3')
+        dotBody.style.backgroundColor = 'red'
+        newTop < 80 ? '80px' : dotBody.style.top = newTop + 'px'
+        dotBody.style.left = newLeft + 'px'
+    } else if (dotBody.classList[0] == 'clicked3') {
+        dotBody.classList.remove('clicked3')
+        dotBody.style.display = 'none'
+        document.querySelector('.first-paragraph').innerHTML = 'Congratulations!'
+    }
 })
 
 dotBody.addEventListener('mouseover', dotMove)
