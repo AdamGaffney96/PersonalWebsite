616e61de8d83efb4b7e0d35b75f47e295a1c240f
Adam Gaffney <adamgaffney96@gmail.com>
2022-02-27 14:01:29 +0000

    general base.html and base.css updates

--- blog_site/urls.py
+++ blog_site/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path, re_path
 from . import views
-from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('', views.home, name = "home"),
-    path()
 ]
